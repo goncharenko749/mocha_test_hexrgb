2a7498703e8aa069d43a69eb0daae741fb68b18b
Alexander <shamp33rr@gmail.com>
2018-04-03 15:31:41 +0300

    fixed

--- hex2rgb.js
+++ hex2rgb.js
@@ -1,27 +1,21 @@
-function convert(hex) {
-
-    const rgb = [];
-    
-    if (!hex.startsWith('#')) {
-    throw "Sorry, I don't know what to do with that value. I'm just a browser :/";
-    }
-    
-    hex = hex.substr(1, 8);
-    
-    
-    for (i = 0; i < 4; i++) {
-    
+function convert(hexInput) {
+  const rgb = [];
+  if (!hexInput.startsWith('#')) {
+    throw Error;
+  }
+  const hex = hexInput.substr(1, 8);
+  for (let i = 0; i < 4; i += 1) {
     if (hex.length === 4) {
-    rgb[i] = hex.substr(i, 1) + hex.substr(i, 1);
+      rgb[i] = hex.substr(i, 1) + hex.substr(i, 1);
     } else {
-    rgb[i] = hex.substr(i * 2, 2);
+      rgb[i] = hex.substr(i * 2, 2);
     }
-    
-    rgb[i] = parseInt(rgb[i], 16);
-    }
-    
-    //output rgb values in a usable format
-    return rgb;
-    }
-    
-    module.exports = convert;
+
+    rgb[i] = hex.length === 4 ?
+      parseInt(hex.substr(i, 1) + hex.substr(i, 1), 16) :
+      parseInt(hex.substr(i * 2, 2), 16);
+  }
+  return rgb;
+}
+
+module.exports = convert;
--- test.js
+++ test.js
@@ -1,32 +1,35 @@
 const assert = require('assert');
 const hex = require('./hex2rgb');
+
 const fixtures = [
   {
     expected: [0xFF, 0xFF, 0xFF, 0xCC],
-    inputs: ['#fffc', '#ffffffcc']
+    inputs: ['#fffc', '#ffffffcc'],
   },
   {
     expected: [0x0F, 0xD1, 0xEA, 0x1F],
-    inputs: ['#0fd1ea1f']
+    inputs: ['#0fd1ea1f'],
   },
   {
     expected: [0, 0, 0, 0xFF],
-    inputs: ['#000f', '#000000ff']
+    inputs: ['#000f', '#000000ff'],
   },
   {
     expected: [0xFF, 0, 0, 0xFF],
-    inputs: ['#f00f', '#ff0000ff']
-  }
-]
- 
-describe('convert test', function () {
-  describe('#parse()', function () {
+    inputs: ['#f00f', '#ff0000ff'],
+  },
+  {
+    expected: [0xFF, 0xFF, 0xFF],
+    inputs: ['#fff', '#ffffff', 'fff', 'ffffff'],
+  },
+];
+
+describe('convert test', () => {
+  describe('#parse()', () => {
     fixtures.forEach(({ expected, inputs }) =>
       inputs.forEach(input =>
-        it(`parse ${input.padEnd(9, ' ')} as ${expected}`, function () {
-          assert.deepEqual(hex(input), expected)
-        })
-      )
-    )
-  })
-})
+        it(`parse ${input.padEnd(9, ' ')} as ${expected}`, () => {
+          assert.deepEqual(hex(input), expected);
+        })));
+  });
+});
